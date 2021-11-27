a1df91f67afad1873fa7c14e030787226ba1b10f
amg7 <57702181+amg7@users.noreply.github.com>
2021-11-27 17:02:19 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u R9dgQipiSGedZy3Hr3MD9WSztRMnG3egw6.amg -p x -t 1
